5613d3ccd14662984c6f0d79f3c8419a6dca1b8c
MikiTakafumi <karugami02468@gmail.com>
2018-11-19 13:32:00 +0900

    add print result by assign

--- report2.py
+++ report2.py
@@ -5,32 +5,69 @@ IF CONST > CONST { IDENT = CONST }
 IF CONST == IDENT { IDENT = CONST EOS IDENT = IDENT EOS }
 IF CONST != IDENT { IDENT = CONST } ELSE { IDENT = CONST + IDENT }
 """
-# IDENT = CONST Optional(opArith IDENT) ->  
-# IF CONST opLogic (CONST | IDENT) lparen IDENT EQ CONST rparen
-# ELSE lparen IDENT opeLogic rparen  
+# if const == ident:
+#   ident = const eos
+#   ident = const eos
+
+# if const != ident:
+#   ident = const eos
+# else:
+#   ident = const + ident eos
 
 data2 = """\
 IF CONST == IDENT IDENT = CONST EOS
 IF CONST != IDENT IDENT = CONST EOS ELSE IDENT = CONST + IDENT EOS
 """
 
-declare   = Literal( 'DECLARE' )
 ident     = Literal( 'IDENT' )
 const     = Literal( 'CONST' )
+eos       = Literal( 'EOS' )
 rsvIf     = Literal( 'IF' )
-revElse   = Literal( 'ELSE')
+rsvElse   = Literal( 'ELSE')
 opAssign  = Literal( '=' )
-lparen    = Literal( '{' )
-rparen    = Literal( '}' )
+# lparen    = Literal( '{' )
+# rparen    = Literal( '}' )
 
 opLogic   = oneOf( '== != <= < >= > && || !')
 opArith   = oneOf( '+ - * / ** %' )
+operator = opLogic ^ opArith
+# 代入文として IDENT = CONST
+# 比較文として CONST > CONST or IDENT
 
-factor  = (const ^ itent) + ope + (const ^ itent)
-term    = factor + ZeroOrMore()
+# IDENT = CONST operator IDENT
+# CONST operator (IDENT ^ CONST)
+# factor -> IDENT opAssign CONST operator (IDENT ^ CONST) 
+factor  = Optional(ident) + Optional(opAssign) + const + Optional(operator) + Optional(ident ^ const)  
+term = factor + ZeroOrMore( operator + factor)
 
-# condition = CONST opLogic (CONST ^ IDENT)
-condition = 
-IF_STMT = 
-ELSE_STMT  =
-# oneof(const | itent) ope oneof(const | itent)
+# condition = const + operator + (CONST ^ IDENT)
+
+
+_if = rsvIf + Optional(term)
+ifS = _if.setResultsName("IF_STMT")
+
+_else = rsvElse + Optional(term)
+elseS = _else.setResultsName("ELSE_STMT")
+
+_assign = ident + opAssign + term
+assignS = _assign.setResultsName('ASN')
+
+statatement = assignS  ^ ifS ^ elseS
+# statatement = ifS ^ elseS
+
+r = statatement
+
+print('line   col  stmt   DSL')
+print('----- ----- ------ ---------------------')
+for i in r.scanString(data2):
+#    print(i)
+    ty=[x for x in i[0].asDict().keys()]
+    if 'IF_STMT' in ty:
+        ty='IF_STMT'
+    elif 'ELSE_STMT' in ty:
+        ty='ELSE_STMT'
+    else: ty=ty[0] 
+    print('%5d %5d %-6s %-s'%(
+            lineno(i[1], data2), col(i[1], data2),
+            ty,
+            ' '.join([x for x in i[0]]) ) )
